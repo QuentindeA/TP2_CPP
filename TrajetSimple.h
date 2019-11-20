600794279766dbf65239a5dfd9fe0ce787ff27b6
Rob Louis <lrob@if501-219-08.insa-lyon.fr>
2019-11-20 17:26:35 +0100

    Creation de TrajetSimple.h et TrajetSimple.cpp et modif du Makefile

---
+++ TrajetSimple.h
@@ -0,0 +1,53 @@
+/*******************************************************************************
+                           TrajetSimple.h  -  description
+                             -------------------
+    début                : 20/11/2019
+    copyright            : (C) 2019 par Louis ROB et Quentin DE ANDRIA
+    e-mail               : louis.rob@insa-lyon.fr quentin.de-andria@insa-lyon.fr
+*******************************************************************************/
+
+//---------- Interface de la classe <TrajetSimple> (fichier TrajetSimple.h) ----------------
+#if ! defined ( TRAJET_SIMPLE_H )
+#define TRAJET_SIMPLE_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include "Trajet.h"
+#include <iostream>
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <TrajetSimple>
+// La classe TrajetSimple permet de créer des trajets simples à partir d'un
+// départ, d'une arrivée et d'un moyen de transport
+//------------------------------------------------------------------------
+
+class TrajetSimple : public Trajet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    void Afficher ( ) const;
+    
+//-------------------------------------------- Constructeurs - destructeur
+    TrajetSimple ( const char* startPoint, const char* endPoint, 
+                   const char* transportMean );
+    
+    virtual ~TrajetSimple ( );
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+    const char * mean;
+};
+
+//-------------------------------- Autres définitions dépendantes de <TrajetSimple>
+
+#endif // TRAJET_SIMPLE_H
+
