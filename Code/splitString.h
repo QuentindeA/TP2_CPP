4e65b04d7cf67a3c557c547dfe0a73c11fd7e46c
lucas <lucaszim16@gmail.com>
2019-12-18 15:40:18 +0100

    splitString.h

---
+++ Code/splitString.h
@@ -0,0 +1,49 @@
+
+#if ! defined ( SPLITSTRING_H )
+#define SPLITSTRING_H
+
+static constexpr auto whitespace = " \t\v\r\n\f";
+static constexpr auto npos = std::string::npos;
+
+std::vector<std::string> split(std::string_view str,
+                                std::string_view sep = "",
+                                std::size_t maxsplit = std::numeric_limits<std::size_t>::max())
+{
+    std::vector<std::string> result;
+    if (sep.empty())
+    {
+        for (std::size_t start = str.find_first_not_of(whitespace), splits = 0; start != npos; ++splits)
+        {
+            if (auto end = str.find_first_of(whitespace, start); end != npos && splits < maxsplit)
+            {
+                result.emplace_back(str.substr(start, end - start));
+                start = str.find_first_not_of(whitespace, end);
+            }
+            else
+            {
+                result.emplace_back(str.substr(start));
+                break;
+            }
+        }
+    }
+    else
+    {
+        for (std::size_t start = 0, splits = 0; start < str.size(); ++splits)
+        {
+            if (auto end = str.find(sep, start); end != npos && splits < maxsplit)
+            {
+                result.emplace_back(str.substr(start, end - start));
+                start = end + sep.size();
+            }
+            else
+            {
+                result.emplace_back(str.substr(start));
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+
+#endif // SPLITSTRING_H
